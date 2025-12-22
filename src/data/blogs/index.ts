0ef35d33ee6270be40121df15e19c73980574e63
Jagan Teki <jagan@edgeble.ai>
2025-12-22 23:43:33 +0530

    blogs: yourstory
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>

--- src/data/blogs/index.ts
+++ src/data/blogs/index.ts
@@ -1,9 +1,11 @@
 import lpc2025 from "./lpc2025";
 import nissan from "./nissan";
 import capgemeni from "./capgemeni";
+import yourstory from "./yourstory";
 
 export const NEW_BLOGS = [
 	lpc2025,
 	nissan,
 	capgemeni,
+	yourstory,
 ];
---
+++ src/data/blogs/yourstory.ts
@@ -0,0 +1,15 @@
+import { BlogPost } from "../blogTypes";
+
+const blog: BlogPost = {
+  title: "Edgeble as Tech30 Startup by YourStory",
+  slug: "yourstory",
+  content: "We’re proud to share that Edgeble has been recognized as one of TOP30 Startups by YourStory Media TechSparks2025 and the only Semiconductor company on the list",
+  author: "Jagan Teki",
+  date: "2025-12-08",
+  readTime: "3 min read",
+  image: "/images/tech30.JPG",
+  tags: ['TechSparks', 'YourStory', 'Tech30', 'Startups'],
+  category: "Recognition",
+};
+
+export default blog;
