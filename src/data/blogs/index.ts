c57d1ec341110392784ab5c7d0b74e2f71eaed76
Jagan Teki <jagan@edgeble.ai>
2025-12-23 18:04:56 +0530

    blog: add tunyml
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>

--- src/data/blogs/index.ts
+++ src/data/blogs/index.ts
@@ -3,6 +3,7 @@ import nissan from "./nissan";
 import capgemeni from "./capgemeni";
 import yourstory from "./yourstory";
 import emva25 from "./emva25";
+import tinyml24 from "./tinyml24";
 
 export const NEW_BLOGS = [
 	lpc2025,
@@ -10,4 +11,5 @@ export const NEW_BLOGS = [
 	capgemeni,
 	yourstory,
         emva25,
+        tinyml24,
 ];
---
+++ src/data/blogs/tinyml24.ts
@@ -0,0 +1,18 @@
+import { BlogPost } from "../blogTypes";
+
+const blog: BlogPost = {
+  title: "Presenting at TinyML 2024, Taipei",
+  slug: "tinyml2024",
+  excerpt: "EdgeMLOps End-to-End Edge AI Lifecycle with On-Device AI",
+  content:
+    "TinyML Taipei 2024 Innovation Showcase provided the perfect venue to demonstrate our end-to-end edge AI lifecycle capabilities, powered by EdgeGPT pre-trained edge AI accelerators",
+  author: "Jagan Teki",
+  date: "2025-12-08",
+  readTime: "3 min read",
+  image: "/images/tinyml25.png",
+  tags: ['TinyML', 'Taipei', 'Edge AI Lifecycle'],
+  type: "blog",
+  category: "Technology",
+};
+
+export default blog;
