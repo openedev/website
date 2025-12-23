409c1dce857d2adfd2f6918ae58a235f72ce9f5d
Jagan Teki <jagan@edgeble.ai>
2025-12-23 16:20:05 +0530

    lpc2025: update
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>

--- src/data/blogs/lpc2025.ts
+++ src/data/blogs/lpc2025.ts
@@ -3,6 +3,7 @@ import { BlogPost } from "../blogTypes";
 const blog: BlogPost = {
   title: "Speaking at LPC2025, Tokyo",
   slug: "lpc2025",
+  excerpt: "Why NPUs need Linux-style contracts, not SDKs.",
   content:
     "GPUs had a decade of evolution in open source. NPUs remain fragmented...",
   author: "Jagan Teki",
